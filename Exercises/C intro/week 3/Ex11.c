9fa00f30ea162670e7e7b3f74a044cb2192e6ffc
AnhLoiChu <chuanhloilee@gmail.com>
2026-09-19 03:18:58 +0700

    done week 3 intro

---
+++ Exercises/C intro/week 3/Ex11.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+printf("ma hp        ten hoc phan      so tin chi     diem \n");
+printf("IT123213       code                2            A");
+return 0;
+
+}
---
+++ Exercises/C intro/week 3/Ex12.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+printf("HOA DON\n");
+printf("Anh san pham       Muc        So luong         Don gia        Thanh tien\n");
+printf("______         Banh chung         1               200              200\n");
+printf("______         Banh cupcake       2              200              400\n");
+printf("______         Banh carot         2               300              600\n");
+printf("                                             Tong so tien           1200\n");
+return 0;
+}
