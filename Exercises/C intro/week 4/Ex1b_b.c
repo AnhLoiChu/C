8aca2c6cc676deaabfb485358309786279fc1469
AnhLoiChu <chuanhloilee@gmail.com>
2026-09-20 20:54:37 +0700

    ex1 week 4

---
+++ Exercises/C intro/week 4/Ex1b_b.c
@@ -0,0 +1,14 @@
+//Code 1b_b
+#include <stdio.h>
+#define MAX_LENGTH 40
+int main() {
+   char str[MAX_LENGTH];
+    int n;
+
+    scanf("%d", &n);
+    fgets(str, MAX_LENGTH, stdin);
+
+    printf("%s\n", str);
+    printf("%d\n", n);
+    return 0;
+}
